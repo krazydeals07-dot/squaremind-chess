4e76cc3ebb6a13d40a53c9b702aeb6c034fc83d6
krazydeals07 <krazydeals07@gmail.com>
2026-04-26 11:52:23 +0000

    Matched appId in capacitor config and android gradle

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.squaremind.app',
+  appId: 'com.squaremind.chess',
   appName: 'SquareMind',
   webDir: 'dist'
 };
