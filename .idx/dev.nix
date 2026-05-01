9f7d1abcfdd44f48cd999f850ab7bfe7543f129e
krazydeals07 <krazydeals07@gmail.com>
2026-05-01 17:52:30 +0000

    Updated app icon

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,9 +1,25 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  # Re-import the original pkgs with 'allowUnfree' enabled.
+  pkgs-with-unfree = import pkgs.path {
+    config.allowUnfree = true;
+    inherit (pkgs) system;
+  };
+in
+{
   channel = "unstable";
+
   packages = [
     pkgs.nodejs_20
+    pkgs-with-unfree.jdk
+    pkgs-with-unfree.android-tools
   ];
-  env = {};
+
+  env = {
+    ANDROID_HOME = "${pkgs-with-unfree.android-tools}";
+    JAVA_HOME = "${pkgs-with-unfree.jdk}";
+  };
+
   idx = {
     extensions = [
       "google.gemini-cli-vscode-ide-companion"
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,24 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.squaremind.chess',
   appName: 'SQUAREMIND',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: false
+    }
+  },
+  cordova: {},
+  android: {
+    path: 'assets'
+  }
 };
 
 export default config;
